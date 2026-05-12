5abf90418a0dde29f1f30a6d47082262fd1bd86d
Yos <127940089+IamYos@users.noreply.github.com>
2026-05-12 12:16:20 +0300

    aksjgdhjw hfoiw

--- src/components/marketing/solutions/SolutionsPage.tsx
+++ src/components/marketing/solutions/SolutionsPage.tsx
@@ -11,7 +11,7 @@ import styles from "./SolutionsPage.module.css";
 
 const FEATURE_START_SCROLL = 1;
 const SOLUTIONS_ENGINE_SRC =
-  "/solutions/home.html?v=softchain-progress-ui-20260507";
+  "/solutions/home.html?v=softchain-progress-ui-20260512-heading-top";
 
 const SOLUTION_ANCHOR_IDS: Record<string, string> = {
   ai: "intuitive",
